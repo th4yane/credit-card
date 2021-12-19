e38621d2b4624cb5f83ad783015defdb4e211e99
Thayane Depaula <thayane.depaula@nubank.com.br>
2021-12-19 17:51:36 -0300

    Funcao para buscar compras por filtro

--- src/credit_card/core.clj
+++ src/credit_card/core.clj
@@ -5,3 +5,11 @@
 (let [compras (get-in c.db/cliente [:cartao :compras])
       resumo (c.logic/resumo-valor-por-categoria compras)]
       (println resumo))
+
+(let [compras (get-in c.db/cliente [:cartao :compras])
+      resumo (c.logic/busca-compra-por-filtro compras :estabelecimento "loja def")]
+  (println resumo))
+
+(let [compras (get-in c.db/cliente [:cartao :compras])
+      resumo (c.logic/busca-compra-por-filtro compras :valor 230.5)]
+  (println resumo))
